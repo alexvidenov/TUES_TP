7be4f8bd4741760ca5e4aa16e8e7611827a93a76
alexvidenov <a.videnov8a@gmail.com>
2019-12-10 14:48:30 +0200

    Additions to the previous Flask app.

---
+++ Flask/main.py
@@ -0,0 +1,32 @@
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return redirect(url_for('list_posts'))
+
+@app.route('/posts')
+def list_posts():
+    return render_template('posts.html', posts=Post.all())
+
+@app.route('/posts/<int:id>')
+def show_post(id):
+    post = Post.find(id)
+
+    return render_template('post.html', post=post)
+
+@app.route('/posts/<int:id>/edit')
+def edit_post(id):
+    post = Post.find(id)
+
+    return render_template('post.html', post=post)
+
+@app.route('/posts/new', methods=['GET', 'POST'])
+def new_post():
+    if request.method == 'GET':
+        return render_template('new_post.html')
+    elif request.method == 'POST':
+        values = (None, request.form['name'], request.form['author'], request.form['content'])
+        Post(*values).create()
+
+        return redirect(url_for('list_posts'))
+
